7d690f425ae6befce0a3891de50e894bd7c6f551
ericsongr <ericson.gramos@yahoo.com>
2020-01-02 23:05:00 +0800

    set to hide navbar, toolbar, footer and sidepanel.

--- RicMonitoringAPI/RicMonitoringUI/src/app/main/apps/online-booking/online-booking/online-booking.component.ts
+++ RicMonitoringAPI/RicMonitoringUI/src/app/main/apps/online-booking/online-booking/online-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FuseConfigService } from '@fuse/services/config.service';
 
 @Component({
   selector: 'app-online-booking',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnlineBookingComponent implements OnInit {
 
-  constructor() { }
+  config: any;
+
+  constructor(private _fuseConfigService: FuseConfigService) {
+
+      this.setConfig();
+  
+  }
+
+  setConfig(){
+    // Configure the layout
+    this._fuseConfigService.config = {
+      layout: {
+          navbar   : {
+              hidden: true
+          },
+          toolbar  : {
+              hidden: true
+          },
+          footer   : {
+              hidden: true
+          },
+          sidepanel: {
+              hidden: true
+          }
+      }
+    };
+  }
 
   ngOnInit() {
+    // Subscribe to config change
+    this._fuseConfigService.config
+    .subscribe((config) => {
+        this.config = config;
+    });
   }
 
 }
