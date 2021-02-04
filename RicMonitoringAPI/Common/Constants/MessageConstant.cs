e373457aa683630016c47a816d1de444ddabe854
Administrator <Administrator@ERICSON>
2021-02-04 15:11:57 +0800

    added error message "Selected room was occupied. Please select available room.".

---
+++ RicMonitoringAPI/Common/Constants/MessageConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace RicMonitoringAPI.Common.Constants
+{
+    public class MessageConstant
+    {
+        public const string OccupiedRoom = "Selected room was occupied. Please select available room.";
+    }
+}
