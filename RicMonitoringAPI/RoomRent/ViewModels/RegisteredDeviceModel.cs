ea344250bccd0a7facd9fe2b4b5dadac73ff36cf
Ericson <ericsongr@yahoo.com>
2022-01-26 21:45:09 +0800

    added TestSendDueDateAlertPushNotification

---
+++ RicMonitoringAPI/RoomRent/ViewModels/RegisteredDeviceModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace RicMonitoringAPI.RoomRent.ViewModels
+{
+    public class RegisteredDeviceModel
+    {
+        public string DeviceId { get; set; }
+        public string AspNetUsersId { get; set; }
+    }
+}
