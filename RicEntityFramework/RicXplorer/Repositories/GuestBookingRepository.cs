c2cfc3781fc24bb6d1cc801e882cf7b96edc8823
Ericson Ramos <ericsongr@yahoo.com>
2022-05-12 17:02:09 +0800

    guest book/save functionality

---
+++ RicEntityFramework/RicXplorer/Repositories/GuestBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using RicEntityFramework.BaseRepository;
+using RicEntityFramework.RicXplorer.Interfaces;
+using RicModel.RicXplorer;
+
+namespace RicEntityFramework.RicXplorer.Repositories
+{
+    public class GuestBookingRepository : EntityBaseRepository<GuestBooking>, IGuestBookingRepository
+    {
+        public GuestBookingRepository(RicDbContext context) : base(context)
+        {
+        }
+    }
+}
