a82b0b3469331a8c5ae1391abe9e4484bbbdd6f0
Ericson Ramos <ericsongr@yahoo.com>
2024-08-06 20:08:43 +0800

    function to update Guest Booking Type api #354

---
+++ RicMonitoringAPI/RicXplorer/ViewModels/GuestBookingTypeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RicMonitoringAPI.RicXplorer.ViewModels
+{
+    public class GuestBookingTypeModel
+    {
+        public int BookingTypeId { get; set; }
+        public bool IsActive { get; set; }
+        public decimal OnlinePrice { get; set; }
+        public decimal Price { get; set; }
+        public int NoOfPersons { get; set; }
+        public int NoOfPersonsMax { get; set; }
+    }
+}
